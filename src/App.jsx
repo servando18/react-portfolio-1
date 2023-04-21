13e0bc4f10259fb473ba6c1d6af346ced498340b
servando18 <100006660+servando18@users.noreply.github.com>
2023-04-20 22:12:50 -0500

    Updated App.jsx
    Updated App.jsx by adding the rest of my page components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/nav'
+import About from './components/about/About'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
       <>
       <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials /> 
+      <Contact />
+      <Footer /> 
       
       </>
-   
   )
 }
 
