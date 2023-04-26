ebd53afb01ce5bbe2b7ee593bab3dec3129131fe
servando18 <100006660+servando18@users.noreply.github.com>
2023-04-25 20:08:52 -0500

    Added an image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 export const header = () => {
   return (
@@ -9,7 +10,11 @@ export const header = () => {
         <h5>Hello I'm </h5>
         <h1>Servando Valenzuela</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        <CTA/>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me"/>
+        </div>
        </div>
     </header>
   )
