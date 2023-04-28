8bf88e6b80aca39472ce965b3c1efb332b514a43
servando18 <100006660+servando18@users.noreply.github.com>
2023-04-27 21:32:15 -0500

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ const HeaderSocials = () => {
     <div className='header__socials'>
         <a href='https://www.linkedin.com/in/servandovalenzuela/'target='_blank'></a>
         <a href='https://github.com/servando18'target='_blank'></a>
-        <a href='https://github.com/servando18'target='_blank'></a>
+        <a href='https://twitter.com/S_valenz_'target='_blank'></a>
     </div>
   )
 }
