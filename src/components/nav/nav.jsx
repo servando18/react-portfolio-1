9ff50fa8a48d676a3bbaaced4b0de1fe5b9f5391
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-06 17:15:16 -0500

    Update nav.jsx

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {SiGooglehome} from 'react-icons/si'
+import {TfiUser} from 'react-icons/tfi'
+import {BiBook} from 'react-icons/bi'
+import {RiFoldersLine} from 'react-icons/ri'
+import {TbMessage} from 'react-icons/tb'
+
+
+
 
 const nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><SiGooglehome/></a>
+      <a href="#about"><TfiUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><RiFoldersLine/></a>
+      <a href="#contact"><TbMessage/></a>
+    </nav>
   )
 }
 
