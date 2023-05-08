23f608a2ba8592004360ffd1a62786bdc651edc8
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-07 21:45:19 -0500

    Update to About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './about.css'
+import me2 from '../../assets/me2.jpg'
+import {FaAward} from 'react-icons/fa'
 
 
 const About = () => {
@@ -11,12 +13,31 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-
+            <img src={me2} alt="About Image" />  
           </div>
         </div>
 
         <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ years Developing</small>
+            </article>
 
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ years Developing</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ years Developing</small>
+            </article>
+            
+          </div>
         </div>
       </div>
     </section>
