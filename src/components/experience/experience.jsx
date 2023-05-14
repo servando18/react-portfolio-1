e969458d4189a1040ce2136a7e05add5ddd358cd
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-13 22:31:42 -0500

    Update experience.jsx
    Front end experience section

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {IoIosCheckmarkCircle} from 'react-icons/io'
 
 const experience = () => {
   return (
@@ -9,9 +10,41 @@ const experience = () => {
 
       <div className="container experience__container">
         <div className="experience__frontend">
+        <h3>Frontend Development</h3>
+        <div className="experience__content">
+          <article className='experience__details'>
+            <IoIosCheckmarkCircle />
+            <h4>HTML</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <IoIosCheckmarkCircle />
+            <h4>CSS</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <IoIosCheckmarkCircle />
+            <h4>JavaScript</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+
+          <article className='experience__details'>
+            <IoIosCheckmarkCircle />
+            <h4>Bootstrap</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <IoIosCheckmarkCircle />
+            <h4>React</h4>
+            <small className="text-light">Experienced</small>
+          </article>
 
         </div>
-         <div className="container__backend">
+      </div>
+          <div className="container__backend">
 
           </div>
       </div>
