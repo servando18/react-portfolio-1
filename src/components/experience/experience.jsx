18af7c918150e8d00371d2eb663bc7de40171277
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-18 21:20:13 -0500

    Update experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -13,33 +13,43 @@ const experience = () => {
         <h3>Frontend Development</h3>
         <div className="experience__content">
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>HTML</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>CSS</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>JavaScript</h4>
             <small className="text-light">Intermediate</small>
+            </div>
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>Bootstrap</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>React</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
 
         </div>
@@ -51,21 +61,27 @@ const experience = () => {
           <h3>Backend Development</h3>
         <div className="experience__content">
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>Node JS</h4>
             <small className="text-light">Experienced</small>
+            </div>     
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
-            <h4>MongoDB</h4>
-            <small className="text-light">Experienced</small>
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
+              <h4>MongoDB</h4>
+              <small className="text-light">Experienced</small>
+            </div>
           </article>
 
           <article className='experience__details'>
-            <IoIosCheckmarkCircle />
+            <IoIosCheckmarkCircle className='experience__details-icon' />
+            <div>
             <h4>MySQL</h4>
             <small className="text-light">Basic</small>
+            </div>  
           </article>
 
         </div>
