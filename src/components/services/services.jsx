0e89b60375f677dc32842d00fe73461eae9b4567
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-20 20:42:12 -0500

    updated services jsx component

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './services.css'
+import {AiOutlineCheckSquare} from 'react-icons/ai'
 
 const services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5> What I offer</h5>
+      <h2>Services</h2> 
+      <div className="container services__content">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
