efac702e9680c2654602ac252ab43cb449c4d5d8
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-20 21:14:42 -0500

    update to services css

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -7,7 +7,7 @@ const services = () => {
     <section id='services'>
       <h5> What I offer</h5>
       <h2>Services</h2> 
-      <div className="container services__content">
+      <div className="container services__container">
         <article className="service">
           <div className="service__head">
             <h3>UI/UX Design</h3>
@@ -18,8 +18,88 @@ const services = () => {
               <AiOutlineCheckSquare className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
             </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
           </ul>
         </article>
+
+        {/* END OF UI/UX */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF WEB DEVELOPMENT */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckSquare className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END of COntent creation */}
       </div>
     </section>
   )
