9a22e3c0a6985a4ada1e6c2c095589d4f0b06b2c
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-22 21:25:19 -0500

    replacing images

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './portfolio.css'
-import img1 from '../../assets/img1.jpg'
+import IMG1 from '../../assets/IMG1.jpg'
+import IMG2 from '../../assets/IMG2.jpg'
 
 export const portfolio = () => {
   return (
@@ -11,12 +12,33 @@ export const portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={img1} alt="" />
+            <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
           <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
           <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
+          <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
+          <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+      
+
       </div>
     </section>
   )
