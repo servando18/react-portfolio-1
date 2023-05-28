46f443339ae81119c39a5276a7adf5e9840d64b9
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-27 21:50:30 -0500

    mapping through an array
    mapped through an array to generate portfolio items using the map method

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,6 +4,32 @@ import IMG1 from '../../assets/IMG1.jpg'
 import IMG2 from '../../assets/IMG2.jpg'
 import IMG3 from '../../assets/IMG3.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'under construction',
+    github: 'https://github.com',
+    demo: ''
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'coming soon',
+    github: 'https://github.com',
+    demo: ''
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'under construction cat',
+    github: 'https://github.com',
+    demo: ''
+  }
+]
+
+
+
 export const portfolio = () => {
   return (
     <section id='portfolio'>
@@ -11,40 +37,22 @@ export const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+          <article key={id} className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
+            <img src={image} alt={title} />
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>{title}</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href={github} className='btn' target='_blank'>Github</a>
+          <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/servando18/react-portfolio-1" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-      
+            )
+          })
+        }
 
       </div>
     </section>
@@ -53,3 +61,6 @@ export const portfolio = () => {
 
 
 export default portfolio 
+
+
+      
