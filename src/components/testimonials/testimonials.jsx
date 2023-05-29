4f36b48096dde8784c608b259d491903227dc7df
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-28 20:52:43 -0500

    updated testimonials jsx

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './testimonials.css'
+import IMG4 from '../../assets/IMG4.png'
+import IMG5 from '../../assets/IMG4.png'
+
 
 const testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+      <div className="client__avatar">
+          <img src="IMG4" alt="IMGFOUR" />
+        
+        </div>
+       </article>
+      </div>
+    </section>
   )
 }
 
