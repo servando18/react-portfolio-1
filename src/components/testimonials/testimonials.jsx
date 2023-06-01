97cec438b9bd4d49c4175c8c6edb6388e4454da8
servando18 <100006660+servando18@users.noreply.github.com>
2023-05-31 21:29:59 -0500

    updated testimonials component
    updated both the jsx and the css

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -34,7 +34,12 @@ const testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <Swiper className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={30}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
         {
          data.map(({avatar, name, review}, index) => {
           return (
