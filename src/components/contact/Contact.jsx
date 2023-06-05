056fefca6948ad3aad57731ba8a6050ae4c6e833
servando18 <100006660+servando18@users.noreply.github.com>
2023-06-04 19:30:20 -0500

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,7 +35,12 @@ const Contact = () => {
 
         </div>
           {/* END OF Contact OPTIONS */}
-        <form action=""></form>    
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>    
       </div>
     </section>
   )
