8f369cf2df7977867061e03b58ded68abecf6321
servando18 <100006660+servando18@users.noreply.github.com>
2023-06-12 21:01:42 -0500

    updates to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,9 @@ export const header = () => {
   return (
     <header>
        <div className="container header__container">
-        <h5>Hello I'm </h5>
+        <h3>Hello I'm </h3>
         <h1>Servando Valenzuela</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h2 className="text-light">Fullstack Developer</h2>
         <CTA />
         <HeaderSocials /> 
 
