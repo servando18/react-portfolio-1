531334e533c5cb1421e611988eca50e1abdd74f8
servando18 <100006660+servando18@users.noreply.github.com>
2023-06-12 21:56:01 -0500

    update to header css

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsTwitter} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/servandovalenzuela'target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com/servando18'target='_blank'><BsGithub/></a>
-        <a href='https://twitter.com/S_valenz_'target='_blank'><BsTwitter/></a>
+        <a href='https://www.linkedin.com/in/servandovalenzuela'target='_blank'><BsLinkedin size={30} color='grey'/></a>
+        <a href='https://github.com/servando18'target='_blank'><BsGithub size={30} color='grey'/></a>
+        <a href='https://twitter.com/S_valenz_'target='_blank'><BsTwitter size={30} color='grey'/></a>
     </div>
   )
 }
